089edaef4e3f0758946f9004f08cb1904b3b677b
Subash <subhashboppana0502@gmail.com>
2023-12-05 23:41:05 -0600

    Removed Comments
    Removed Comments

--- index.js
+++ index.js
@@ -4,22 +4,20 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Set EJS as the view engine
 app.set('view engine', 'ejs');
 
-// Set the views directory
+
 app.set('views', path.join(__dirname, 'views'));
 
-// Serve static files (images in this case)
+
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-// Define a route
+
 app.get('/', (req, res) => {
-  // Render the 'index' view with a function to generate random prices
   res.render('home1', { getRandomPrice });
 });
 
-// Function to generate random prices
+
 function getRandomPrice() {
   return (Math.random() * (100 - 10) + 10).toFixed(2);
 }
@@ -89,7 +87,7 @@ app.get("/orderhistory", function(req,res){
   res.render("orderhistory");
 })
 
-// Start the server
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
